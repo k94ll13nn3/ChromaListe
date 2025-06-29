512351b6199269aca12a5680d566982f13e252f0
Kévin Gallienne <k94ll13nn3@outlook.fr>
2025-06-29 18:28:44 +0200

    Add description to balls

--- src/ChromaListe.Web/Core/Ball.cs
+++ src/ChromaListe.Web/Core/Ball.cs
@@ -2,29 +2,54 @@ namespace ChromaListe.Web.Core;
 
 public enum Ball
 {
+    [ChromaDescription("Poké")]
     Poke = 0,
+    [ChromaDescription("Super")]
     Great = 1,
+    [ChromaDescription("Hyper")]
     Ultra = 2,
+    [ChromaDescription("Master")]
     Master = 3,
+    [ChromaDescription("Filet")]
     Net = 4,
+    [ChromaDescription("Scuba")]
     Dive = 5,
+    [ChromaDescription("Faiblo")]
     Nest = 6,
+    [ChromaDescription("Bis")]
     Repeat = 7,
+    [ChromaDescription("Chrono")]
     Timer = 8,
+    [ChromaDescription("Luxe")]
     Luxury = 9,
+    [ChromaDescription("Honor")]
     Premier = 10,
+    [ChromaDescription("Sombre")]
     Dusk = 11,
+    [ChromaDescription("Soin")]
     Heal = 12,
+    [ChromaDescription("Rapide")]
     Quick = 13,
+    [ChromaDescription("Speed")]
     Fast = 14,
+    [ChromaDescription("Niveau")]
     Level = 15,
+    [ChromaDescription("Appât")]
     Lure = 16,
+    [ChromaDescription("Masse")]
     Heavy = 17,
+    [ChromaDescription("Love")]
     Love = 18,
+    [ChromaDescription("Copain")]
     Friend = 19,
+    [ChromaDescription("Lune")]
     Moon = 20,
+    [ChromaDescription("Compét")]
     Sport = 21,
+    [ChromaDescription("Rêve")]
     Dream = 22,
+    [ChromaDescription("Ultra")]
     Beast = 23,
+    [ChromaDescription("Safari")]
     Safari = 24,
 }
